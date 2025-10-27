da1ecaff0013191e8d64f630a79f3438225b40af
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-10-27 23:39:07 +0600

    HelloandNamePrinter executed

---
+++ Java Basic Programming/Basic Part-l/HelloandNamePrinter.java
@@ -0,0 +1,13 @@
+/*
+Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Sourav Mondal Shuvo
+ */
+
+public class HelloandNamePrinter {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Sourav Mondal Shuvo");
+    }
+}
