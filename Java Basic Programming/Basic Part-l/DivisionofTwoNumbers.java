b1bd320fec1660ae5d88de79b8783995912ae543
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-10-27 23:49:33 +0600

    DivisionofTwoNumbers

---
+++ Java Basic Programming/Basic Part-l/DivisionofTwoNumbers.java
@@ -0,0 +1,17 @@
+/* 
+Write a Java program to divide two numbers and print them on the screen.
+Test Data :
+50/3
+Expected Output :
+16
+*/
+
+public class DivisionofTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 50;
+        int num2 = 3;
+        int division = num1 / num2;
+        System.out.print("Division of " + num1 + " by " + num2 + " is: ");
+        System.out.println(division);
+    }
+}
