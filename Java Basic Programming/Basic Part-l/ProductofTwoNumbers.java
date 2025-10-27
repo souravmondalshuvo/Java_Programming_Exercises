346faa86349328dd3623fd14d9cb2460f0c06a57
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-10-28 00:08:55 +0600

    ProductofTwoNumbers executed

---
+++ Java Basic Programming/Basic Part-l/ProductofTwoNumbers.java
@@ -0,0 +1,17 @@
+/* 
+Write a Java program that takes two numbers as input and displays the product of two numbers.
+Test Data:
+Input first number: 25
+Input second number: 5
+Expected Output :
+25 x 5 = 125
+*/
+
+public class ProductofTwoNumbers {
+    public static void main(String[] args) {
+        int first_number = 25;
+        int second_number = 5;
+        int product = first_number * second_number;
+        System.out.println(first_number + " X " + second_number + " = " + product);
+    }
+}
