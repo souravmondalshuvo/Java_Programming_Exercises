889bd4a5d476b07f68a6bc87c1ec5eac3eb24ba4
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-10-27 23:45:53 +0600

    SumofTwoNumbers executed

---
+++ Java Basic Programming/Basic Part-l/SumofTwoNumbers.java
@@ -0,0 +1,17 @@
+/*
+Write a Java program to print the sum of two numbers.
+Test Data:
+74 + 36
+Expected Output :
+110
+*/
+
+public class SumofTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 74;
+        int num2 = 36;
+        int sum = num1 + num2;
+        System.out.print("Sum of " + num1 + " and " + num2 + " is: ");
+        System.out.println(sum);
+    }
+}
