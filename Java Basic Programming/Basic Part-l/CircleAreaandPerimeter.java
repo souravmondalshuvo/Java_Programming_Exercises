9c0a8417e0fc657be329f10db7d0cbaccdc0fb8d
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-10-28 01:18:25 +0600

    CircleAreaandPerimeter executed

---
+++ Java Basic Programming/Basic Part-l/CircleAreaandPerimeter.java
@@ -0,0 +1,21 @@
+/*
+Write a Java program to print the area and perimeter of a circle.
+Test Data:
+Radius = 7.5
+Expected Output
+Perimeter is = 47.12388980384689
+Area is = 176.71458676442586
+*/
+
+public class CircleAreaandPerimeter {
+    public static void main(String[] args) {
+        
+        double radius = 7.5;
+
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+}
