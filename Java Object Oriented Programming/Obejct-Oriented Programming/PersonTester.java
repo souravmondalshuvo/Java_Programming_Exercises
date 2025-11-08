f6aca87fbdc85cc60df598082906e3cb0568b6db
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 07:03:31 +0600

    Object-Oriented Programming PersonTester.java updated

--- Java Object Oriented Programming/Obejct-Oriented Programming/PersonTester.java
+++ Java Object Oriented Programming/Obejct-Oriented Programming/PersonTester.java
@@ -1,3 +1,5 @@
+//Write a Java program to create a class called "Person" with a name and age attribute. Create two instances of the "Person" class, set their attributes using the constructor, and print their name and age.
+
 public class PersonTester {
     public static void main(String[] args) {
             
