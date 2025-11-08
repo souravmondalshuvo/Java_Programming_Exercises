8fc4e7d10565b678b0eca2a0b94895c62f9c18a5
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 07:26:30 +0600

    Object-Oriented Programming Rectangle.java executed

---
+++ Java Object Oriented Programming/Obejct-Oriented Programming/RectangleTester.java
@@ -0,0 +1,17 @@
+//Write a Java program to create a class called "Rectangle" with width and height attributes. Calculate the area and perimeter of the rectangle.
+
+public class RectangleTester {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(7, 12);
+
+        System.out.println("The area of the rectangle is " + rectangle.getArea());
+        System.out.println("The perimeter of the rectangle is " + rectangle.getPerimeter());
+
+        rectangle.setWidth(6);
+        rectangle.setHeight(12);
+
+        System.out.println("\nThe area of the rectangle is now " + rectangle.getArea());
+        System.out.println("\nThe perimeter of the rectangle is now " + rectangle.getPerimeter());
+    }
+}
