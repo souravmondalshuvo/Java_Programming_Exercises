f9af2fde03c13ec9a3b08abced90c381b1d61acd
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 07:00:08 +0600

    Object-Oriented Programming executed

--- Java Object Oriented Programming/Obejct-Oriented Programming/PersonTester.java
+++ Java Object Oriented Programming/Obejct-Oriented Programming/PersonTester.java
@@ -1,29 +1,3 @@
-class Person {
-    private  String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
-
 public class PersonTester {
     public static void main(String[] args) {
             
