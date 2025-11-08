4c9cebf4647b4330b0cc8fd130d2c5f93cf8e92d
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 07:14:38 +0600

    Object-Oriented Programming Dog.java executed

--- Java Object Oriented Programming/Obejct-Oriented Programming/DogTester.java
+++ Java Object Oriented Programming/Obejct-Oriented Programming/DogTester.java
@@ -4,7 +4,7 @@ public class DogTester {
     public static void main(String[] args) {
         
         Dog dog1 = new Dog("Buddy", "Golden Retriever");
-        Dog dog1 = new Dog("Charlie", "Bulldog");
+        Dog dog2 = new Dog("Charlie", "Bulldog");
         
         System.out.println(dog1.getName() + " is a " + dog1.getBreed() + ".");
         System.out.println(dog2.getName() + " is a " + dog2.getBreed() + ".\n");
