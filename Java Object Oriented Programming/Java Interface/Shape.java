8c16a8d5e04eb616a95862fce797607b4b5843cf
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 06:46:01 +0600

    Java Interface executed

---
+++ Java Object Oriented Programming/Java Interface/Shape.java
@@ -0,0 +1,8 @@
+// Shape.java
+// This is an interface named 'Shape' that defines a contract for classes to implement.
+
+public interface Shape {
+    
+    // Abstract method signature for getting the area of a shape.
+    double getArea();
+}
