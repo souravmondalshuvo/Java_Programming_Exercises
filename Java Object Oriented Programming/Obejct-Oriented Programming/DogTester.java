41120afbc5cc439618a16563e4e852fabecb1083
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-08 07:10:13 +0600

    Object-Oriented Programming DogTester.java executed

---
+++ Java Object Oriented Programming/Obejct-Oriented Programming/DogTester.java
@@ -0,0 +1,19 @@
+//Write a Java program to create a class called "Dog" with a name and breed attribute. Create two instances of the "Dog" class, set their attributes using the constructor and modify the attributes using the setter methods and print the updated values.
+
+public class DogTester {
+    public static void main(String[] args) {
+        
+        Dog dog1 = new Dog("Buddy", "Golden Retriever");
+        Dog dog1 = new Dog("Charlie", "Bulldog");
+        
+        System.out.println(dog1.getName() + " is a " + dog1.getBreed() + ".");
+        System.out.println(dog2.getName() + " is a " + dog2.getBreed() + ".\n");
+
+        System.out.println("Set the new Breed of dog1 and new Name of dog2: ");
+        dog1.setBreed("Labrador Retriever");
+        dog2.setName("Daisy");
+
+        System.out.println(dog1.getName() + " is a " + dog1.getBreed() + ".");
+        System.out.println(dog2.getName() + " is a " + dog2.getBreed() + ".\n");
+    }    
+}
