079f12253f59f35d7af9cd4b3426d76c8791d3ab
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2025-11-18 15:36:24 +0600

    Object-Oriented Programming CardTester.java updated

--- Java Object Oriented Programming/Obejct-Oriented Programming/MFS.java
+++ Java Object Oriented Programming/Obejct-Oriented Programming/MFS.java
@@ -20,12 +20,15 @@ public class MFS {
     }
 
     public void addMoneyFromCard(double amount) {
+        
         if (this.card == null) {
             System.out.println("Please add a card to your MFS account");
         } else {
+            
             if (this.card.balance >= amount) {
                 this.card.balance -= amount;
                 this.balance += amount;
+                
                 System.out.println("Transfer successful.");
                 System.out.println("New MFS account balance: " + this.balance + " tk.");
                 System.out.println("Card balance: " + this.card.balance + " tk");
