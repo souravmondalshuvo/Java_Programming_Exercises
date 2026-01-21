983ce9f52083c8e26a1473e65f51c82626548d31
Sourav Mondal Shuvo <souravmondal033@gmail.com>
2026-01-22 02:59:23 +0600

    loops for ArrayList updated

--- Java Data Structures (DS)/ArrayLists.java
+++ Java Data Structures (DS)/ArrayLists.java
@@ -63,8 +63,10 @@ public class ArrayLists {
 
         //loops for ArrayList
         for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            System.out.print(list.get(i) + " ");
         }
+
+        System.out.println();
 
         System.out.println("=======================");
     }
